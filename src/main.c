61e646d367b29698bdc9e83a63fc56c31e928abf
Ric <rtoledocrow@gmail.com>
2025-03-31 18:28:05 -0400

    more cleaning

--- src/main.c
+++ src/main.c
@@ -19,13 +19,12 @@ static struct k_thread uart_thread;
 static struct k_thread https_thread;
 static struct k_thread data_acq_thread;
 
-static const struct device *gpio_port = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+//static const struct device *gpio_port = DEVICE_DT_GET(DT_NODELABEL(gpio0));
 
 extern void uart_thread_entry(void *, void *, void *);
 extern void https_thread_entry(void *, void *, void *);
 extern void data_acq_entry(void *, void *, void *);
 
-extern const struct device *const my_uart0;
 extern const struct device *const my_uart1;
 
 int main(void) {
@@ -46,17 +45,15 @@ int main(void) {
 	}
 	
 	/*Initialize Pin2*/
-	printf(" initializing gpio\n");
+	/* printf(" initializing gpio\n");
 	ret = gpio_pin_configure(gpio_port, 2, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		printf("ret=%d",ret);
 		return -1;
 	}
-	
+	// turn gpio pin 2 on
 	gpio_pin_set(gpio_port,2,1);
-	printk("pin 2 on\n");	
-	
-
+	printk("pin 2 on\n");	*/
 
 	/* Start UART thread */
 	k_thread_create(&uart_thread, uart_thread_stack,
